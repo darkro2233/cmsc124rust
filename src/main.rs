5655dab6fe1ec544be971252032fb0ab699f69d9
Ken Villanueva <kbvillanueva5@up.edu.ph>
2019-10-25 19:10:57 +0800

    v1

---
+++ src/main.rs
@@ -0,0 +1,57 @@
+use std::io;
+
+struct Subject{
+	class_section:String,
+	course_code: String
+}
+
+struct Student{
+	name: String,
+	s_num: String,
+	subjects: Vec<Subject>
+}
+
+fn addStudent(){
+	
+}
+
+fn viewStudents(){
+
+}
+
+fn addSubject(){
+
+}
+
+fn deleteStudent(){
+
+}
+
+fn main(){
+	let v:Vec<Student> = Vec::new();
+	let 
+	loop{
+		let mut input; 
+
+		println!("========== MENU ==========
+			
+			[1] Add student
+			[2] View ALL students
+			[3] Add subject to student
+			[4] Delete student
+			[5] Exit
+			
+			==========================");
+
+		match input {
+			1 => 
+			2 =>
+			3 =>
+			4 =>
+			5 =>
+			_ => 
+		
+		}
+	}
+
+}
